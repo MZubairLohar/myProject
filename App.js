d956a72d66143c383f9d5b12f2782f1a879f418c
MZubairLohar <zubairgituser@gmail.com>
2020-06-04 20:21:16 +0500

    update app.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-natiVe';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, World!</Text>
+      </View>
     </View>
   );
 }
@@ -16,4 +18,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'green',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight:'bold',
+  }
 });
