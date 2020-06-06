0a6fbc3a790dc28a646921feb5fe99fb9c6c7d20
MZubairLohar <zubairgituser@gmail.com>
2020-06-06 17:21:39 +0500

    clickHandler to update state

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Zubair');
+
+  const clickHandler = () => {
+    setName ('Lohar');
+  } 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
+     <Text>My name is {name}</Text>
+     <View style={styles.buttonContainer}>
+       <Button title='update state' onPress={clickHandler}/>
+     </View>
+     
+      {/* <View style={styles.header}>
         <Text style={styles.boldText}>React Native</Text>
       </View>
       <View style={styles.body}>
         <Text style={styles.body}>bold Text</Text>
         <Text>My First Project</Text>
-      </View>
+      </View> */}
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -21,16 +32,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor: 'green',
-    padding: 10,
-  },
-  boldText: {
-    fontWeight:'bold',
-  },
-  body: {
-    backgroundColor: 'blue',
-    padding: 20,
-    fontWeight:'bold'  //it wont work until declear it in text tag
+  // header: {
+  //   backgroundColor: 'green',
+  //   padding: 10,
+  // },
+  // boldText: {
+  //   fontWeight:'bold',
+  // },
+  // body: {
+  //   backgroundColor: 'blue',
+  //   padding: 20,
+  //   fontWeight:'bold'  //it wont work until declear it in text tag
+  // }
+  buttonContainer:{
+    marginTop:10 ,
   }
 });
