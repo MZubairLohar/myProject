d050b299172872f243845c7a6febe24c7692dbac
MZubairLohar <zubairgituser@gmail.com>
2020-06-08 13:04:11 +0500

    scroll view & array

--- App.js
+++ App.js
@@ -1,9 +1,42 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { StyleSheet, Text, View, ScrollView} from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('Zubair');
-  const [age, setAge] = useState('30');
+  const[people, setPeople] = useState (
+    [
+      {name: 'Zubair', key:'1'},
+      {name: 'Umair', key:'2'},
+      {name: 'Ubaid', key:'3'},
+      {name: 'Imran', key:'4'},
+      {name: 'Ashir', key:'5'},
+      {name: 'shabbir', key:'6'},
+      {name: 'Kashif', key:'7'},
+      {name: 'Bilal', key:'8'},
+      {name: 'faisal', key:'9'}
+      
+    ]);
+  return(
+    
+    <View style={styles.container}>
+      <ScrollView>
+      {people.map((item) =>{
+        return(
+          <View key={item.key}>
+            <Text style={styles.item}>
+              {item.name}</Text>
+             
+          </View>
+          
+        )
+      })}
+           </ScrollView>
+    </View>
+   
+  );
+    };
+  
+  // const [name, setName] = useState('Zubair');
+  // const [age, setAge] = useState('30');
   //arry type
   //const [person, setPerson] = useState({name : 'eman' , age: 30});
 
@@ -11,27 +44,27 @@ export default function App() {
   //   setName ('Lohar');
   //   setPerson ({name:'chodri', age: 31});
   // } 
-  return (
-    <View style={styles.container}>
-     <Text>Enter your Name:</Text>
-      <TextInput 
-      multiline
-      style={styles.input}
-      placeholder='e.g. jhon'
-      onChangeText={(val) =>setName(val)}/>
-      <Text>Enter your age:</Text>
-      <TextInput 
-      keyboardType='numeric'
-      style={styles.input}
-      placeholder='e.g. 20'
-      onChangeText={(val) =>setAge(val)}/>
+//   return (
+//     <View style={styles.container}>
+//      <Text>Enter your Name:</Text>
+//       <TextInput 
+//       multiline
+//       style={styles.input}
+//       placeholder='e.g. jhon'
+//       onChangeText={(val) =>setName(val)}/>
+//       <Text>Enter your age:</Text>
+//       <TextInput 
+//       keyboardType='numeric'
+//       style={styles.input}
+//       placeholder='e.g. 20'
+//       onChangeText={(val) =>setAge(val)}/>
 
 
-  <Text>name:{name}, age: {age}</Text>
+//   <Text>name:{name}, age: {age}</Text>
     
-    </View>
-  );
-}
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -55,14 +88,20 @@ const styles = StyleSheet.create({
   // buttonContainer:{
   //   marginTop:10 ,
   // },
-  input:{
-    borderWidth:1,
-    borderColor: '#777',
-    padding:8,
-    margin: 10,
-    width:200,
+  // input:{
+  //   borderWidth:1,
+  //   borderColor: '#777',
+  //   padding:8,
+  //   margin: 10,
+  //   width:200,
 
 
+  // }
+  item:{
+    margin:15,
+    padding:15,
+    backgroundColor:'green',
+    fontSize:24
   }
 
 });
